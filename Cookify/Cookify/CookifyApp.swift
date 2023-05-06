1c14e6c1dafb74116030e0f66ee38137dff55301
Artem Prischepov <artem.prishepov@gmail.com>
2023-05-06 14:52:46 +0300

    init commit

---
+++ Cookify/Cookify/CookifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookifyApp.swift
+//  Cookify
+//
+//  Created by Artem Prishepov on 6.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CookifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
